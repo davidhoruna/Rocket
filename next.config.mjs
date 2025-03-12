82f2c502c951125e918950f023ec65403c646b64
David H. <d.huamanor@alum.up.edu.pe>
2025-03-11 21:01:15 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
+const nextConfig = {
+        eslint: {
+          // Warning: This allows production builds to successfully complete even if
+          // your project has ESLint errors.
+          ignoreDuringBuilds: true,
+        },
+};
+
+export default nextConfig;
